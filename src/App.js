6edd059528da5264d811cecbf52e6cab174b1301
LYH513 <peperony367@gmail.com>
2024-11-24 00:01:36 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import {Route, Routes} from "react-router-dom"
 import List from './components/List';
 import Detail from './components/Detail';
@@ -68,27 +67,6 @@ function App() {
         server={server}
       />}></Route>
     </Routes>
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> abab79d (Initialize project using Create React App)
   );
 }
 
